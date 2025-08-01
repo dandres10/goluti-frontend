924181824d5e6c2b6819ef1b01d56b67560e4b5f
andres <dandresleon64@gmail.com>
2025-07-31 22:58:09 -0500

    ajuste para acceder desde el celular

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,18 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:8000',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
